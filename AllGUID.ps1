fc75d9cf8922cfd627cb10899550febd3ee99281
DavidVezina <david.vezina@kohezio.com>
2025-01-16 17:08:38 -0500

    Add files via upload

---
+++ AllGUID.ps1
@@ -0,0 +1,8 @@
+﻿$GUID = Get-Content -Path "C:\temp\GUID.txt"
+
+foreach ($userGUID in $GUID) {
+$aad_immutableid = "$userGUID"
+$targetConsistencyGuid = [Convert]::FromBase64String($aad_immutableid)
+$targetConsistencyGuid_string = $targetConsistencyGuid -join ' '
+write-host $targetConsistencyGuid_string
+}
